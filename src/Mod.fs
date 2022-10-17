4b2e5c550e152b19e744b7af6de0f06b7ad04047
Timo Vahtera <timo.vahtera@gmail.com>
2022-10-17 21:03:12 +0300

    Move mod related functions under Mod module

--- src/Mod.fs
+++ src/Mod.fs
@@ -1,7 +1,10 @@
 module xcommander.Mod
+open System
 open System.IO
 open xcommander.Utility.Regex
 open xcommander.Utility.String
+open Configuration
+open Utility.File
 
 type Mod = {
     Path : string
@@ -33,7 +36,6 @@ let loadMod path  =
       RequiresXPACK   = matches |> tryGetCapturedSubstring "RequiresXPACK" |> Option.map (trim >> bool.Parse)
       Tags            = matches |> tryGetCapturedSubstring "tags" |> Option.map trim
       ContentImage    = matches |> tryGetCapturedSubstring "contentImage" |> Option.map trim }
-      
 
 let toString modification =
     sprintf "Title: %s \nPath: %s \nPublishedFileId: %s \nDescription: %s \nRequiresXPACK: %A \nTags: %A \nContentImage: %A \nCategory: %A \n"
@@ -47,3 +49,43 @@ let toString modification =
         modification.Category
 
 let getTitle m = m.Title
+
+let enabled =
+    Paths.ModOptionsFile
+    |> File.ReadAllLines
+    |> Array.where (startsWith "ActiveMods=")
+    |> Array.map (replace "ActiveMods=" String.Empty)
+
+let isEnabled m = enabled |> Array.contains m.Name
+let isDisabled m = isEnabled m |> not
+
+let all = 
+    Paths.WorkshopContentFolder
+    |> fun dir -> Directory.EnumerateFiles(dir, "*.XComMod", SearchOption.AllDirectories)
+    |> Seq.map loadMod
+    |> Seq.map (fun x -> x.Name, x)
+    |> Map.ofSeq
+
+let disable m =
+    Paths.ModOptionsFile
+    |> File.ReadAllLines
+    |> fun lines -> lines |> Array.tryFindIndex (startsWith $"ActiveMods={m.Name}"), lines
+    |> function
+    | Some index, lines ->
+        Array.removeAt index lines |> writeAllLines Paths.ModOptionsFile
+        Ok $"{m.Title} disabled."
+    | _ -> Error $"{m.Title} is already disabled."
+
+let enable m =
+    match all |> Map.tryFind m.Name with
+    | None -> Error $"No such mod as {m.Title} is downloaded."
+    | Some m when isEnabled m -> Error $"{m.Title} is already enabled."
+    | _ -> 
+        Paths.ModOptionsFile 
+        |> File.ReadAllLines
+        |> fun lines -> lines, lines |> Array.tryFindIndexBack (startsWith "ActiveMods=") |> function | Some i -> i + 1 | None -> 1 
+        |> fun (lines, insertIndex) -> 
+            lines
+            |> Array.insertAt insertIndex $"ActiveMods={m.Name}"
+            |> writeAllLines Paths.ModOptionsFile
+            Ok $"{m.Title} activated"
--- src/Program.fs
+++ src/Program.fs
@@ -1,54 +1,10 @@
 ﻿open System
-open System.IO
 open xcommander.Configuration
-open xcommander.Utility.String
 open xcommander.Utility.Process
-open xcommander.Utility.File
 open xcommander.Utility.Result
-open xcommander.Mod
+open xcommander
 
 open type Environment
-
-let enabledMods =
-    Paths.ModOptionsFile
-    |> File.ReadAllLines
-    |> Array.where (startsWith "ActiveMods=")
-    |> Array.map (replace "ActiveMods=" String.Empty)
-
-let isEnabled m = enabledMods |> Array.contains m.Name
-let isDisabled m = isEnabled m |> not
-
-let mods = 
-    Paths.WorkshopContentFolder
-    |> fun dir -> Directory.EnumerateFiles(dir, "*.XComMod", SearchOption.AllDirectories)
-    |> Seq.map loadMod
-    |> Seq.map (fun x -> x.Name, x)
-    |> Map.ofSeq
-
-let disableMod m =
-    Paths.ModOptionsFile
-    |> File.ReadAllLines
-    |> fun lines -> lines |> Array.tryFindIndex (startsWith $"ActiveMods={m.Name}"), lines
-    |> function
-    | Some index, lines ->
-        Array.removeAt index lines |> writeAllLines Paths.ModOptionsFile
-        Ok $"{m.Title} disabled."
-    | _ -> Error $"{m.Title} is already disabled."
-
-let enableMod m =
-    match mods |> Map.tryFind m.Name with
-    | None -> Error $"No such mod as {m.Title} is downloaded."
-    | Some m when isEnabled m -> Error $"{m.Title} is already enabled."
-    | _ -> 
-        Paths.ModOptionsFile 
-        |> File.ReadAllLines
-        |> fun lines -> lines, lines |> Array.tryFindIndexBack (startsWith "ActiveMods=") |> function | Some i -> i + 1 | None -> 1 
-        |> fun (lines, insertIndex) -> 
-            lines
-            |> Array.insertAt insertIndex $"ActiveMods={m.Name}"
-            |> writeAllLines Paths.ModOptionsFile
-            Ok $"{m.Title} activated"
-
 
 Console.OutputEncoding <- Text.Encoding.UTF8
 match GetCommandLineArgs() |> Array.tryItem 1, GetCommandLineArgs() |> Array.tryItem 2 with
@@ -57,32 +13,33 @@ match GetCommandLineArgs() |> Array.tryItem 1, GetCommandLineArgs() |> Array.try
 
 // Enable arguments
 | Some "enable", Some "*" -> 
-    mods
-    |> Map.values 
-    |> Seq.where isDisabled 
-    |> Seq.map enableMod
+    Mod.all
+    |> Map.values
+    |> Seq.where Mod.isDisabled 
+    |> Seq.map Mod.enable
     |> Seq.iter print
 
 | Some "enable", Some name -> 
-    name
-    |> mods.TryFind
+    Mod.all
+    |> Map.tryFind name
     |> function
-        | Some name -> enableMod name
+        | Some name -> Mod.enable name
         | None -> Error $"No such mod as {name} is downloaded."
     |> print
 
 // Disable arguments
 | Some "disable", Some "*" -> 
-    mods 
+    Mod.all 
     |> Map.values 
-    |> Seq.where isEnabled
-    |> Seq.map disableMod
+    |> Seq.where Mod.isEnabled
+    |> Seq.map Mod.disable
     |> Seq.iter print
+
 | Some "disable", Some name -> 
-    name
-    |> mods.TryFind
+    Mod.all
+    |> Map.tryFind name
     |> function
-        | Some name -> disableMod name
+        | Some name -> Mod.disable name
         | None -> Error $"No such mod as {name} is downloaded."
     |> print
 
@@ -90,20 +47,20 @@ match GetCommandLineArgs() |> Array.tryItem 1, GetCommandLineArgs() |> Array.try
 | Some "list", None -> printfn "Valid list parameters are:\n   all\n   enabled\n   disabled\n   downloaded"
 | Some "list", Some "enabled" ->
     printfn "Listing enabled mods..."
-    mods.Values 
-    |> Seq.where isEnabled
-    |> Seq.map getTitle
+    Mod.all.Values 
+    |> Seq.where Mod.isEnabled
+    |> Seq.map Mod.getTitle
     |> Seq.iter (printfn " • %s")
 | Some "list", Some "disabled" ->
     printfn "Listing disabled mods..."
-    mods.Values 
-    |> Seq.where isDisabled
-    |> Seq.map getTitle
+    Mod.all.Values 
+    |> Seq.where Mod.isDisabled
+    |> Seq.map Mod.getTitle
     |> Seq.iter (printfn " • %s")
 | Some "list", Some "all" -> 
     printfn "Listing all mods..."
-    let longestTitleLength  = mods |> Map.values |> Seq.map (getTitle >> String.length) |> Seq.max
-    mods |> Map.iter (fun k m -> printfn " %s %-*s     (%s)" (if isEnabled m then "•" else " ") longestTitleLength m.Title k)
+    let longestTitleLength  = Mod.all |> Map.values |> Seq.map (Mod.getTitle >> String.length) |> Seq.max
+    Mod.all |> Map.iter (fun k m -> printfn " %s %-*s     (%s)" (if Mod.isEnabled m then "•" else " ") longestTitleLength m.Title k)
 
 | Some "run", _ -> run Paths.ExecutableFile Paths.WorkingDirectory config.LaunchArguments
 
