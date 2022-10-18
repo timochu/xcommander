08232907a48d36eb7a44b8463a1f3f73684a3bb6
Timo Vahtera <timo.vahtera@gmail.com>
2022-10-18 12:21:51 +0300

    Change String.contains to be case insensitive

--- src/Utility.fs
+++ src/Utility.fs
@@ -4,7 +4,7 @@ module String =
     let replace oldValue (newValue : string) (str : string) = str.Replace(oldValue, newValue)
     let trim (s : string) = s.Trim()
     let startsWith (value : string) (s : string) = s.StartsWith(value)
-    let contains (value : string) (s : string) = s.Contains(value)
+    let contains (value : string) (s : string) = s.Contains(value, System.StringComparison.InvariantCultureIgnoreCase)
 
 module Process =
     let run filename workingDirectory arguments = 
